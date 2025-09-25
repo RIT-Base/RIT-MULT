b6b72a0cf89a8ea1dc7d6e2342ca0b56b99748a2
asymmetry1 <kuronamiku@gmail.com>
2025-09-25 18:47:53 +0700

    Add: New Feature

---
+++ tools/word_counter.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+
+router = APIRouter()
+
+class ExampleInput(BaseModel):
+    text: str
+
+# Contoh endpoint (hapus/ubah sesuai kebutuhan)
+@router.post("/word_count")
+async def example_endpoint(payload: ExampleInput):
+    """
+    Word Counter
+    """
+    text = payload.text.strip()
+    
+    words = text.split()
+    word_count = len(words)
+    char_count = len(text)
+
+    return {
+        "text": text,
+        "word_count": word_count,
+        "char_count": char_count,
+    }
+
